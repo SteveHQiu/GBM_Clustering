d47043b26a147727259759ed62d2e552e5abb8ea
EndorphinSponge <hqiu1@ualberta.ca>
2023-02-27 01:08:01 -0500

    -Reformatted tempFx to report rates of first and non-first cancers -Added function to report incidence rate ratio across age distribution

--- processing.py
+++ processing.py
@@ -10,11 +10,15 @@ import pandas as pd
 from pandas import DataFrame, Series
 import matplotlib.pyplot as plt
 
+from internals import LOG
+
 
 #%% Constants
+ROOT_PATH = R"C:\Users\steve\Downloads\ZLocal\SEER RPD 17 Nov 2021.csv"
 ROOT_PATH = R"data/SEER RPD 17 Nov 2021.csv"
 ROOT_NAME = os.path.splitext(ROOT_PATH)[0]
 COUNTERS_PATH = R"data/counters.dat"
+COL_AGE = "Age recode with <1 year olds"
 COL_SITE = "Site recode ICD-O-3/WHO 2008"
 COL_HIST = "Histologic Type ICD-O-3"
 COL_TYPE = "ICD-O-3 Hist/behav"
@@ -27,82 +31,133 @@ COL_PRIMARY = "Primary by international rules" # Whether its primary or mets
 COL_ORD_PRIM = "Sequence number" # Order of the primary AT THE TIME OF DIAGNOSIS, should only have 1-2 primaries when first diagnosed (rarely should you be walking in with 3+ primaries without being diagnosed) FIXME UNSURE HOW IT CHANGES WITH EACH ENTRY, SHOULD DOUBLE CHECK FOR A SPECIFIC PATIENT WITH MULTIPLE ENTRIES
 GBM_HIST_CODES = [9440, 9441, 9442, 9445]
 
+EXPORT_CHECKPOINTS = 0
+
 #%% Load CSV
 df = pd.read_csv(ROOT_PATH)
 df[COL_SURV] = pd.to_numeric(df[COL_SURV], errors="coerce") # Coerce converts non-numerics into NaN
-#%%
+n_encatchment = 81885000 # SEER RPD 17 Nov 2021 should have ~81,885,000 total encatchment
+n_years = 20 # Also remember that SEER RPD 17 Nov 2021 is cumulative over 20 years 
+n_total = df[COL_ID].nunique()
+
 if 0: # Visualize variable space of df
     for col in df.columns:
-        print(col)
-        print(df[col].unique())
-#%% Get different rates of GBM 
+        LOG.info(col)
+        LOG.info(df[col].unique())
+#%% Get GBM records
 
-def tempFx(df: DataFrame, frac_gen = 0.000556793063442633):
-    df_gbm = df.loc[df[COL_HIST].isin(GBM_HIST_CODES)] # df.isin() method used since "in" operator doesn't work in this context
-    n_total = df[COL_ID].nunique()
-    n_gbm = df_gbm[COL_ID].nunique()
-    n_encatchment = 81885000 # SEER RPD 17 Nov 2021 should have ~81,885,000 total encatchment
-    n_years = 20 # Also remember that SEER RPD 17 Nov 2021 is cumulative over 20 years 
-    print(F'Number of patients: {n_gbm}') 
-    print(F'Fraction of encatchment with GBM: {n_gbm / n_encatchment}\nn = {n_gbm}, N = {n_encatchment}') 
-    print(F'GBM rate per 100 000 per year: {n_gbm / (n_encatchment / 100000) / n_years}') # Should be roughly 3.19
+df_gbm = df.loc[df[COL_HIST].isin(GBM_HIST_CODES)] # df.isin() method used since "in" operator doesn't work in this context
+n_gbm = df_gbm[COL_ID].nunique()
 
-    df_gbm_first = df_gbm[df_gbm[COL_ORD_PRIM].str.contains('One primary only|1st of 2 or more primaries')]
-    n_gbm_first = df_gbm_first[COL_ID].nunique() 
-    # df_gbm_first.to_excel(F"{ROOT_NAME}_gbm_first.xlsx")
-    assert n_gbm_first == len(df_gbm_first) # Each entry should be unique
+LOG.info(F'Number of patients: {n_gbm}') 
+LOG.info(F'Fraction of encatchment with GBM: {n_gbm / n_encatchment}\nn = {n_gbm}, N = {n_encatchment}') 
+LOG.info(F'GBM rate per 100 000 per year: {n_gbm / (n_encatchment / 100000) / n_years}') # Should be roughly 3.19
+
+if EXPORT_CHECKPOINTS:
+    df_gbm.to_excel(F"{ROOT_NAME}_gbm.xlsx")
+
+
+#%%
+
+def reportCancerIncidence(df: DataFrame,
+           hist_codes = GBM_HIST_CODES,
+           prefix = "gbm",
+           incidence_20y_first = 0.000556793,
+           export_dfs = False,
+           ):
+    # Get primary and secondary rates for a target cancer in a DataFrame
+    # Target cancer by histology codes
+    # frac_gen is prevalence of target cancer as first cancer (all occurences minus secondary ocurrences)
+    # frac_gen calculated by (n_gbm - n_gbm_sec) / n_encatchment
+    
+    df_target_cancer = df.loc[df[COL_HIST].isin(hist_codes)] # df.isin() method used since "in" operator doesn't work in this context
+    
+    # Find first primaries of target cancer and their patients' related entries
+    df_first = df_target_cancer[df_target_cancer[COL_ORD_PRIM].str.contains('One primary only|1st of 2 or more primaries')]
+    n_first = df_first[COL_ID].nunique() 
+    if export_dfs:
+        df_first.to_excel(F"{ROOT_NAME}_{prefix}_first.xlsx")
+    
+    df_gbm_first_rel = df[df[COL_ID].isin(df_first[COL_ID])]
+    if export_dfs:
+        df_gbm_first_rel.to_excel(F"{ROOT_NAME}_{prefix}_first_rel.xlsx")
+    
+    # Debug
+    if not n_first == len(df_first): # Each entry in first entry should be unique
+        LOG.warning(F"Not all first tumour entries are unique\nLength: {len(df_first)} | Unique: {n_first}")
 
     if 0: 
-        for pt_id in df_gbm_first[df_gbm_first[COL_SEQ] != 1][COL_ID]: # Output number of entries for patients where their first GBM primary was not the first SEER entry, FIXME Not sure why there should be any of these in the first place
-            print(len(df[df[COL_ID] == pt_id]))
-
-    df_rem = df[COL_ID].isin(df_gbm_first[COL_ID])
+        for pt_id in df_first[df_first[COL_SEQ] != 1][COL_ID]: # Output number of entries for patients where their first GBM primary was not the first SEER entry, FIXME Not sure why there should be any of these in the first place
+            LOG.info(len(df[df[COL_ID] == pt_id]))
     
-    df_gbm_first_rel = df[df[COL_ID].isin(df_gbm_first[COL_ID])]
-    # df_gbm_first_rel.to_excel(F"{ROOT_NAME}_gbm_first_rel.xlsx")
+    # Define entries to be removed to get primaries of target cancer that were not the first primaries 
+    ids_entr_rel_to_first = df[COL_ID].isin(df_first[COL_ID])
+    df_not_first = df.drop(df[ids_entr_rel_to_first].index)
+    LOG.info(F"Entries to remove: {ids_entr_rel_to_first.value_counts()[True]}")
+    LOG.info(F"Number of entries removed: {len(df) - len(df_not_first)}")
+    n_not_first = df_not_first[COL_ID].nunique()
+
+    # Find second+ primaries of target cancer and their patients' related entries
+    df_second = df_not_first.loc[df_not_first[COL_HIST].isin(hist_codes)]
+    n_gbm_sec = df_second[COL_ID].nunique()
+    if export_dfs:
+        df_second.to_excel(F"{ROOT_NAME}_{prefix}_second.xlsx")
     
-    n_rem_entries = df_rem.value_counts()[True] # Number of entries to remove
-    print(F"Entries to remove: {n_rem_entries} | Excess: {n_rem_entries - len(df_gbm_first)}")
-    df_no_gbm_first = df.drop(df[df_rem].index)
-    print(F"Number of entries removed: {len(df) - len(df_no_gbm_first)}")
-    n_no_gbm_first = df_no_gbm_first[COL_ID].nunique() 
+    df_gbm_sec_rel = df[df[COL_ID].isin(df_second[COL_ID])]
+    if export_dfs:
+        df_gbm_sec_rel.to_excel(F"{ROOT_NAME}_{prefix}_sec_rel.xlsx")
 
-    df_gbm_sec = df_no_gbm_first.loc[df_no_gbm_first[COL_HIST].isin(GBM_HIST_CODES)]
-    n_gbm_sec = df_gbm_sec[COL_ID].nunique()
-    # df_gbm_sec.to_excel(F"{ROOT_NAME}_gbm_second.xlsx")
+    # Calculate rates
+    if not incidence_20y_first: # If general rate of GBM not given, then define it here
+        incidence_20y_first = (n_gbm - n_gbm_sec) / n_encatchment
+        LOG.info(F'Fraction of encatchment with primary GBM: {incidence_20y_first}\nn = {(n_gbm - n_gbm_sec)}, N = {n_encatchment}') 
+    else:    
+        LOG.info(F'Imported 20 year incidence: {incidence_20y_first}') # Need % of encatchment fitting subpopulation of df to calculate 
     
-    df_gbm_sec_rel = df[df[COL_ID].isin(df_gbm_sec[COL_ID])]
-    df_gbm_sec_rel.to_excel(F"{ROOT_NAME}_gbm_sec_rel.xlsx")
-
-    if not frac_gen: # If general rate of GBM not given, then define it here
-        frac_gen = (n_gbm - n_gbm_sec) / n_encatchment
-    frac_sec = n_gbm_sec / n_no_gbm_first
-
-    print(F'Fraction of encatchment with primary GBM: {frac_gen}\nn = {(n_gbm - n_gbm_sec)}, N = {n_encatchment}') 
-    print(F"Fraction of secondary GBM: {frac_sec}\nn = {n_gbm_sec}, N = {n_no_gbm_first}")
-    print(F">>> Ratio: {frac_sec/frac_gen} <<<")
-
-tempFx(df)
+    incidence_20y_sec = n_gbm_sec / n_not_first # Rate of target cancer as non-first cancer within those with cancer over the past 20 years
+    ratio = incidence_20y_sec/incidence_20y_first
+    
+    LOG.info(F"Fraction of secondary GBM: {incidence_20y_sec}\nn = {n_gbm_sec}, N = {n_not_first}")
+    LOG.info(F">>> Ratio: {ratio} <<<")
+    
+    return ratio
 #%%
+reportCancerIncidence(df, incidence_20y_first=None, export_dfs=True)
+
+#%% Age
+
+cancer_sites = list(df[COL_AGE].unique())
+cancer_sites.sort()
+site_ratios = []
+for col in cancer_sites:
+    df_temp = df[df[COL_AGE] == col]
+    LOG.info(F"Age: {col} ================================")
+    site_ratios.append(reportCancerIncidence(df_temp)) # Add ratio 
+
+fig, ax = plt.subplots()
+ax.bar(cancer_sites, site_ratios)
+ax.set_ylabel("Ratio of non-first GBM occurence rate")
+fig.autofmt_xdate(rotation=45)
+    
+#%% Site
+
+#%% 
 
 for col in df["Sex"].unique():
     df_temp = df[df["Sex"] == col]
-    print(F"Sex: {col} ================================")
-    tempFx(df_temp)
-    
-for col in df["Age recode with <1 year olds"].unique():
-    df_temp = df[df["Age recode with <1 year olds"] == col]
-    print(F"Age: {col} ================================")
-    tempFx(df_temp)
+    LOG.info(F"Sex: {col} ================================")
+    reportCancerIncidence(df_temp)
+
+
 #%%
 df_temp = df[df["Age recode with <1 year olds"].isin(['75-79 years', '80-84 years', '85+ years'])]
-tempFx(df_temp)
+reportCancerIncidence(df_temp)
 #%%
 df_temp = df[(df["Sex"] == "Male") & (df["Age recode with <1 year olds"].isin(['75-79 years', '80-84 years', '85+ years']))]
-tempFx(df_temp)
+reportCancerIncidence(df_temp)
 #%%
 df_temp = df[(df["Sex"] == "Male") & (df["Age recode with <1 year olds"].isin(['75-79 years']))]
-tempFx(df_temp)
+reportCancerIncidence(df_temp)
 #%%
 
 
@@ -121,9 +176,9 @@ def tempFx2(df: DataFrame, frac_gen = 0.000556793063442633):
     # df_rem = df[COL_ID].isin(df_gbm_first[COL_ID])
     
     # n_rem_entries = df_rem.value_counts()[True] # Number of entries to remove
-    # print(F"Entries to remove: {n_rem_entries} | Excess: {n_rem_entries - len(df_gbm_first)}")
+    # LOG.info(F"Entries to remove: {n_rem_entries} | Excess: {n_rem_entries - len(df_gbm_first)}")
     # df_no_gbm_first = df.drop(df[df_rem].index)
-    # print(F"Number of entries removed: {len(df) - len(df_no_gbm_first)}")
+    # LOG.info(F"Number of entries removed: {len(df) - len(df_no_gbm_first)}")
     # n_no_gbm_first = df_no_gbm_first[COL_ID].nunique() 
     n_cases = df[COL_ID].nunique()
 
@@ -131,8 +186,8 @@ def tempFx2(df: DataFrame, frac_gen = 0.000556793063442633):
 
     frac_sec = n_gbm_sec / n_cases
 
-    print(F"Fraction of secondary GBM: {frac_sec}\nn = {n_gbm_sec}, N = {n_cases}")
-    print(F">>> Ratio: {frac_sec/frac_gen} <<<")
+    LOG.info(F"Fraction of secondary GBM: {frac_sec}\nn = {n_gbm_sec}, N = {n_cases}")
+    LOG.info(F">>> Ratio: {frac_sec/frac_gen} <<<")
 
 
 df_pros_entries = df[df["Primary Site - labeled"] == 'C61.9-Prostate gland']
@@ -143,11 +198,11 @@ df_prost = df[df[COL_ID].isin(df_pros_entries2[COL_ID])]
 df_temp = df_prost[(df_prost["Sex"] == "Male") & (df_prost["Age recode with <1 year olds"].isin(['75-79 years']))]
 tempFx2(df_temp)
 
-
+# ====================== Original pipeline 
 #%% Count sites and types for entries 
-
-df_gbm2 = df.loc[df["SEER Brain and CNS Recode"] == "1.1.2 Glioblastoma"] # Alternative way to get GBM
-print(df_gbm2["Patient ID"].nunique())
+df_gbm = df.loc[df[COL_HIST].isin(GBM_HIST_CODES)] # df.isin() method used since "in" operator doesn't work in this context
+df_gbm = df_gbm.loc[df_gbm["Radiation recode"].isin(['None/Unknown', 'Refused (1988+)'])]
+df_gbm = df_gbm.loc[df_gbm["Chemotherapy recode (yes, no/unk)"].isin(['No/Unknown'])]
 
 gbm_ids = set(df_gbm["Patient ID"].values)
 df_gbm_pt = df.loc[df["Patient ID"].isin(gbm_ids)]
@@ -328,3 +383,4 @@ print(df_gbm.groupby(["Sequence number"])["Patient ID"].count())
 # Check if they had GBM, compare GBM "Record number recode" with max
 
 # Can probably compare rates using chi-squared https://www.medcalc.org/calc/rate_comparison.php
+# %%
